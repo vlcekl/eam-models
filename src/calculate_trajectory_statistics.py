27e31f22ccc28d5d62f52cf30c4eb6ab162adf51
Lukas Vlcek <lukas_vlcek@yahoo.com>
2019-02-26 17:39:30 -0500

    updated stats script

--- src/calculate_trajectory_statistics.py
+++ src/calculate_trajectory_statistics.py
@@ -1,5 +1,9 @@
+#!/usr/bin/python
+
 from __future__ import print_function, unicode_literals
 from __future__ import absolute_import, division
+from __future__ import unicode_literals
+
 try:
     xrange = xrange
     # We have Python 2
@@ -20,31 +24,46 @@ sys.path.append('../../statmechlib')
 from statmechlib.preprocessing import Trajectory
 from statmechlib.preprocessing import force_targ, get_stats_EAM_per_atom
 
+#sc = list(np.round(np.linspace(2.02, 5.82, 96), 2))
+sc = list(np.round(np.linspace(1.0, 5.85, 98), 2))
+sc.extend([ 2.45, 2.5648975, 2.6297950, 2.6946925,
+            2.8663175, 2.9730450, 3.0797725, 3.5164725,
+            3.8464450, 4.1764175, 4.7008450, 4.8953000,
+            5.0897550, 5.3429525, 5.4016950, 5.4604375])
+
+sc.sort()
+
+print(sc)
+
+#name = 'marinica'
+name_in = 'all_samples'
 
 # Define locations of relevant datasets
 
-target_raw = '../data/target_raw'
-target_proc = '../data/target_processed'
 working = '../data/working'
+trjfile = 'trj_' + name_in + '.pickle'
+
+name = 'pb_samples'
 
 
 # read dict with trajectories information
-with open(os.path.join(working, "trj_fit.pickle"), 'rb') as fi:
+with open(os.path.join(working, trjfile), 'rb') as fi:
     trj_fit = pickle.load(fi)
 
 
 # Prepare target_data dict
 
-weights = {k:1.0 for k in trj_fit}
-weights['relax'] = 10.0
-weights['eos_bcc'] = 10
-weights['eos_fcc'] = 0.5
+#weights = {k:1.0 for k in trj_fit}
+weights = {}
+for key in trj_fit.keys():
+    weights[key] = 1.0
 
 target_data = {}
 
 for key, trj in trj_fit.items():
     
     print('dataset #', key)
+    sys.stdout.flush()
 
     # target data
     target_dict = {'type':'trajectory'}
@@ -68,17 +87,9 @@ for key, trj in trj_fit.items():
     target_data[key] = target_dict
 
 # save target data
-with open(os.path.join(working, "target_samples.pickle"), 'wb') as fo:
+with open(os.path.join(working, "target_" + name + ".pickle"), 'wb') as fo:
     pickle.dump(target_data, fo, protocol=2)
 
-
-#sc = [2.4, 2.45, 2.5, 2.65, 2.70, 2.72, 2.73, 2.74, 2.75, 2.77, 2.80, 2.85, 2.90,
-#      3.0, 3.1, 3.2, 3.3, 3.45, 3.6, 3.75,
-#      4.0, 4.1, 4.25, 4.356, 4.5, 4.65, 4.8,
-#      5.0, 5.15, 5.3, 5.45, 5.6, 5.75]
-#sc = [2.74, 3.252, 3.804, 4.356, 4.908, 5.46]
-
-sc = list(np.linspace(2.38, 5.78, 86))
 
 # Prepare stats_data dict using multiprocessing
 
@@ -94,6 +105,7 @@ pool = mp.Pool()
 for key, trj in trj_fit.items():
     
     print('dataset #', key)
+    sys.stdout.flush()
 
     configs = zip(trj['xyz'], trj['box'])
 
@@ -102,8 +114,8 @@ for key, trj in trj_fit.items():
     # statistics data
     stats_dict = {'energy':[], 'forces':[]}
     
-    for (a1, ar, a2, ax, f1, fr, f2) in output_stats:
-        stats_dict['energy'].append([ar, a2, a1, ax])
+    for (a1, ar, a2, ax, f1, fr, f2, c1) in output_stats:
+        stats_dict['energy'].append([ar, a2, a1, ax, c1])
         stats_dict['forces'].append([fr, f2, f1])
 
     stats_data[key] = stats_dict
@@ -111,6 +123,6 @@ for key, trj in trj_fit.items():
 pool.close()
 pool.join()
 
-with open(os.path.join(working, "stats_samples.pickle"), 'wb') as fo:
+with open(os.path.join(working, "stats_" + name + ".pickle"), 'wb') as fo:
     pickle.dump(stats_data, fo, protocol=2)
 
--- src/statmechlib/statmechlib/read_write/__init__.py
+++
@@ -1,2 +0,0 @@
-from .read_vasp import read_vasp, read_poscar, read_xdatcar, read_outcar
-#from .write_eam_lammps import write_eam_lammps
